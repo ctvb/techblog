eed682d527aa29512ff2572b5ed1aee966fdc009
ctvb <deenicks@hotmail.com>
2023-04-28 23:31:22 -0500

    tutor help

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const routes = require('./routes');
-const exphbs = require('express-handlebars');
 const path = require('path');
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-// import sequelize connection
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,29 +15,18 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
-const createChatMessage = async (text, user_id) => {
-    try {
-        await Chat.create({
-            text: text,
-            user_id: user_id
-        });
-    } catch (error) {
-        console.error(error);
-    }
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
@@ -48,19 +37,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    console.log('Database Synced');
-    // app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Server Error');
+  app.listen(PORT, () => console.log('Now listening'));
 });
