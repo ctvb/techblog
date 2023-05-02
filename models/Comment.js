9c185c118c0adc7c4ce3dd5ebe296f394a3ad5e8
ctvb <deenicks@hotmail.com>
2023-05-01 23:42:42 -0500

    recommit

--- models/Comment.js
+++ models/Comment.js
@@ -14,25 +14,6 @@ Comment.init(
         comment: {
             type: DataTypes.STRING,
         },
-        date_created: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'post',
-                key: 'id',
-            },
-        },
     },
     {
         sequelize,
